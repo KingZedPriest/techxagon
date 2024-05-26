26141516bfedb82afa65a96e16890cb8c49d3abb
Priest <Charleschukwuemeka47@gmail.com>
2024-05-26 16:03:59 +0100

    Feat: Add Zod for data validation

--- src/components/Authentication/Login.tsx
+++ src/components/Authentication/Login.tsx
@@ -13,15 +13,18 @@ const Login = () => {
             <div className="bg-primary rounded-[50%] size-20 items-center justify-center flex relative z-[2]">
                 <User size={40} className="text-white"/>
             </div>
-            <div className="bg-gray-300 rounded-[2rem] p-4 md:p-6 xl:p-10 -mt-10 shadow-xl relative z-[1]">
-                <div className="flex my-14 items-center justify-center">
-                    <div className="p-2.5 sm:p-3 bg-primary mt-1"><Sms className="text-5xl md:text-7xl xl:text-9xl" color="#FFF"/></div>
-                    <Input type="email" placeholder="Enter your email address" id="email"/>
+            <form className="flex flex-col items-center">
+                <div className="bg-gray-300 rounded-[2rem] p-4 md:p-6 xl:p-10 -mt-10 shadow-xl relative z-[1]">
+                    <div className="flex my-14 items-center justify-center">
+                        <div className="p-2.5 sm:p-3 bg-primary mt-1"><Sms className="text-5xl md:text-7xl xl:text-9xl" color="#FFF"/></div>
+                        <Input type="email" placeholder="Enter your email address" id="email"/>
+                    </div>
                 </div>
-            </div>
-            <div className="bg-gray-300 text-primary rounded-[2rem] -mt-10 w-[60%] md:w-4/5 text-center shadow-xl text-xs sm:text-sm xl:text-base">
-                <input type="submit" value="LOGIN" className="hover:text-white duration-300 cursor-pointer mt-14 mb-4 font-bold tracking-widest"/>
-            </div>
+                <div className="bg-gray-300 text-primary rounded-[2rem] -mt-10 w-[60%] md:w-4/5 text-center shadow-xl text-xs sm:text-sm xl:text-base">
+                    <input type="submit" value="LOGIN" className="hover:text-white duration-300 cursor-pointer mt-14 mb-4 font-bold tracking-widest"/>
+                </div>
+            </form>
+            
         </div>
      );
 }
