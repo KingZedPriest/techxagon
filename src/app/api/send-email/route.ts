9781f52e47db01e6230fea09e6f02ae1c9574c29
Priest <Charleschukwuemeka47@gmail.com>
2024-06-06 23:55:54 +0100

    Feat: Add the email verification for login template

---
+++ src/app/api/send-email/route.ts
@@ -0,0 +1,44 @@
+//Templates
+import LoginAuthenticationTemplate from "../../../../emails/LoginAuthenticationTemplate";
+
+//Needed Utils
+import { render } from "@react-email/render";
+import { NextResponse } from "next/server";
+import { sendEmail } from "@/lib/email";
+
+
+export async function POST(request: Request) {
+    const body = await request.json();
+    try {
+        const { to, subject, emailType } = body;
+
+        if (!to || !subject || !emailType ) throw new Error('Fill in the fields')
+
+        let emailHtml;
+
+        switch (emailType) {
+          
+          case "userLoginVerification":
+            emailHtml = render(LoginAuthenticationTemplate({}));
+            break;
+          
+          default:
+            throw new Error('Invalid emailType');
+        }
+
+      await sendEmail({
+        to,
+        subject,
+        html: emailHtml,
+      });
+
+      return new NextResponse('Email Send Successfully', { status: 200 })
+
+    }catch (error) {
+        if (error instanceof Error) {
+          return new NextResponse(error.message);
+        } else {
+          return new NextResponse('Internal Server Error', { status: 500 });
+        }
+      }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,3 +6,57 @@ export default function Home() {
     </main>
   );
 }
+
+
+
+//Validation of data using Zod, inorder to maintain data types
+
+
+// import { NextResponse, NextRequest } from 'next/server';
+// import { prisma } from '@/lib/prismadb';
+// import { z } from 'zod';
+
+// const BeneficiarySchema = z.object({
+//   userEmail: z.string().email(),
+//   name: z.string(),
+//   bankName: z.string(),
+//   accountName: z.string(),
+//   accountNumber: z.string(),
+//   iban: z.string().optional(),
+//   swiftcode: z.string().optional(),
+//   routingNumber: z.string().optional(),
+// });
+
+// export async function POST(request: NextRequest) {
+//   try {
+//     const body = await request.json();
+//     const parseResult = BeneficiarySchema.safeParse(body);
+
+//     if (!parseResult.success) {
+//       return NextResponse.json({ error: 'Invalid input', details: parseResult.error.errors }, { status: 400 });
+//     }
+
+//     const { userEmail, name, bankName, accountName, accountNumber, iban, swiftcode, routingNumber } = parseResult.data;
+
+//     const lowercasedEmail = userEmail.toLowerCase();
+
+//     const createBeneficiary = await prisma.beneficiary.create({
+//       data: {
+//         userEmail: lowercasedEmail,
+//         name,
+//         bankName,
+//         accountName,
+//         accountNumber,
+//         iban,
+//         swiftcode,
+//         routingNumber,
+//       },
+//     });
+
+//     return NextResponse.json(createBeneficiary);
+
+//   } catch (error) {
+//     console.error('Error creating beneficiary:', error);
+//     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+//   }
+// }
