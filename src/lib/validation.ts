317bd5e4e70ac5f80b91f27027a307ea330e98a1
Priest <Charleschukwuemeka47@gmail.com>
2024-06-14 20:14:26 +0100

    Fix: Add necessary edits

--- src/lib/validation.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const emailSchema = z.object({
-  email: z.string().min(1, 'Email address is required').email('Invalid email address'),
-  subject: z.string().optional()
-});
-
-
-export type FormData = z.infer<typeof emailSchema>
